2fd82993ef551b4fbb6d8d13291fd379c7ce479c
Isa Reinert <reinert.isa@gmail.com>
2024-11-26 12:11:20 -0800

    fix(api): initialize spring boot project

---
+++ cts-api/src/main/java/com/reinertisa/cts/CtsApiApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtsApiApplication.class, args);
+    }
+
+}
---
+++ cts-api/src/test/java/com/reinertisa/cts/CtsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.cts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
