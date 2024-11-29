54b68ea088dfadb2cfea9ebf2262038abfcadcbe
Isa Reinert <reinert.isa@gmail.com>
2024-11-29 00:17:06 -0800

    fix(api): add jsonbackreference to techstack

--- cts-api/src/main/java/com/reinertisa/cts/model/TechStack.java
+++ cts-api/src/main/java/com/reinertisa/cts/model/TechStack.java
@@ -1,7 +1,11 @@
 package com.reinertisa.cts.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter @Setter @Builder
 @AllArgsConstructor @NoArgsConstructor
@@ -18,4 +22,8 @@ public class TechStack {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private TechStackCategory category;
+
+    @JsonBackReference
+    @ManyToMany(mappedBy = "techStacks", fetch = FetchType.LAZY)
+    private Set<Company> companies = new HashSet<>();
 }
