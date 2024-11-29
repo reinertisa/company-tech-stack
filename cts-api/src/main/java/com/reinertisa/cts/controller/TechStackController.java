a51f0cd70859611df003a214a87c26e1f34dac72
Isa Reinert <reinert.isa@gmail.com>
2024-11-29 00:16:31 -0800

    fix(api): update tech stack controller path

--- cts-api/src/main/java/com/reinertisa/cts/controller/TechStackController.java
+++ cts-api/src/main/java/com/reinertisa/cts/controller/TechStackController.java
@@ -14,7 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping(name = "/api/v1/techStacks")
+@RequestMapping(value = "/api/v1/techStacks")
 @RequiredArgsConstructor
 @CrossOrigin
 public class TechStackController {
