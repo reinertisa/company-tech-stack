ea602e84180f1817c65f9ff9f91310ef03fadc16
Isa Reinert <reinert.isa@gmail.com>
2024-11-30 23:05:40 -0800

    fix(website): update company form

--- cts-website/src/components/edit/Company/Form.js
+++ cts-website/src/components/edit/Company/Form.js
@@ -1,11 +1,25 @@
-import {useForm} from 'react-hook-form';
+import {useForm, useFieldArray} from 'react-hook-form';
+import {map} from 'lodash';
 import axios from 'axios';
 
 export default function CompanyForm() {
-    const {register, handleSubmit} = useForm({
+    const {register, handleSubmit, control} = useForm({
         defaultValues: {
-            name: '',
+            name: 'Linkedin',
+            companyId: 'L1234',
+            address: 'Sunnyvale, CA',
+            numOfEmployees: 100,
+            industry: 'Social',
+            type: 'MULTINATIONAL',
+            techStacks: [{
+                name: '',
+                category: ''
+            }]
         }
+    });
+
+    const {fields, append, remove} = useFieldArray({
+        name: 'techStacks', control
     });
 
     const onSubmit = async (data) => {
@@ -20,16 +34,7 @@ export default function CompanyForm() {
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label htmlFor="name">Company name: </label>
-                <input
-                    id="name"
-                    type="text"
-                    {...register('name', {
-                        required: {
-                            value: true,
-                            message: 'This field is required.'
-                        }
-                    })}
-                />
+                <input id="name" type="text"{...register('name')}/>
             </div>
 
             <div>
@@ -41,18 +46,42 @@ export default function CompanyForm() {
                 <label htmlFor="address">Address: </label>
                 <input id="address" type="text" {...register("address")} />
             </div>
+
             <div>
                 <label htmlFor="numOfEmployees">Number of employees: </label>
                 <input id="numOfEmployeess" type="text" {...register("numOfEmployees")} />
             </div>
+
             <div>
                 <label htmlFor="industry">Industry: </label>
                 <input id="industry" type="text" {...register("industry")} />
             </div>
+
             <div>
                 <label htmlFor="type">Company Type: </label>
                 <input id="type" type="text" {...register("type")} />
             </div>
+
+            <div>
+                <label htmlFor="techStacks">Tech Name: </label>
+                {map(fields, (field, index) => {
+                    return (
+                        <div key={field.id}>
+                            <input type="text" {...register(`techStacks.${index}.name`)} />
+                            <input type="text" {...register(`techStacks.${index}.category`)} />
+                            {index > 0 && (
+                                    <button type="button" onClick={() => remove({name: ''})}>
+                                        Remove
+                                    </button>
+                                )
+                            }
+                        </div>
+                    )
+
+                })}
+                <button type="button" onClick={() => append({name: ''})}>Add tech stack</button>
+            </div>
+
             <div>
                 <button type="submit">Submit</button>
             </div>
